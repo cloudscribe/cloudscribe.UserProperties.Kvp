1eab4804450cc69ad6ba989d8f4f8c60466f4ff9
Jim Kerslake <39943820+JimKerslake@users.noreply.github.com>
2022-10-31 10:29:13 +0000

    #40 timestamp migrations

---
+++ src/cloudscribe.Kvp.Storage.EFCore.PostgreSql/Migrations/20221031101240_cs-kvp-timestamps.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace cloudscribe.Kvp.Storage.EFCore.PostgreSql.Migrations
+{
+    public partial class cskvptimestamps : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+            @"DO $$
+            BEGIN
+                ALTER TABLE kvp_items
+                  ALTER created_utc TYPE timestamptz USING created_utc AT TIME ZONE 'UTC'
+                , ALTER created_utc SET DEFAULT now();
+
+                ALTER TABLE kvp_items
+                  ALTER modified_utc TYPE timestamptz USING modified_utc AT TIME ZONE 'UTC'
+                , ALTER modified_utc SET DEFAULT now();
+            END$$;");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
